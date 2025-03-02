dd2e471a57566013eac4dea0cd7f2aecf7eeec5c
Flávio Farias <Fs.flaviosilv4@gmail.com>
2025-03-02 12:50:21 -0300

    Responsividade

--- src/components/categories/listCategories/CategoryList.tsx
+++ src/components/categories/listCategories/CategoryList.tsx
@@ -3,7 +3,7 @@ import { AuthContext } from "../../../contexts/AuthContext";
 import { Search } from "../../../services/Service";
 
 import Category from "../../../models/Category";
-import CategoryBlock from "../categoryBlock/CategoryBlock_temp";
+import CategoryBlock from "../categoryBlock/CategoryBlock";
 
 function CategoryList() {
     const [category, setCategory] = useState<Category[]>([]);
